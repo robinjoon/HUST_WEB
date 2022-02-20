b38e26cef327527b47cea31d4cf4a955e8f71109
robinjoon <0108dlatnqls@gmail.com>
2022-02-21 00:10:50 +0900

    생성자 추가

--- java/vo/Board.java
+++ java/vo/Board.java
@@ -1,13 +1,19 @@
 // 게시판의 정보를 저장하는 클래스
 package vo;
 
+import javax.servlet.http.HttpServletRequest;
+
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@RequiredArgsConstructor
 public class Board implements VO{ 
 	
+	@NonNull
 	private String board_name; //게시판 이름
 	private String board_description; // 게시판 설명
 	private Permission writePermission;
@@ -15,6 +21,20 @@ public class Board implements VO{
 	private Permission managePermission;
 	private Permission commentPermission;
 	
+	public Board(HttpServletRequest request) throws NumberFormatException, Exception {
+		String board_name=request.getParameter("board_name");
+		Permission rp =	Permission.intToPermission(Integer.parseInt(request.getParameter("rp")));
+		Permission wp = Permission.intToPermission(Integer.parseInt(request.getParameter("wp")));
+		Permission mp = Permission.intToPermission(Integer.parseInt(request.getParameter("mp")));
+		Permission cp = Permission.intToPermission(Integer.parseInt(request.getParameter("cp")));
+		String board_description = request.getParameter("board_description");
+		this.setBoard_name(board_name);
+		this.setReadPermission(rp);
+		this.setWritePermission(wp);
+		this.setManagePermission(mp);
+		this.setCommentPermission(cp);
+		this.setBoard_description(board_description);
+	}
 	
 	@Override
 	public String toJson() {
