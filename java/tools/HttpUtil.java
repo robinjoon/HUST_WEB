b29fdb6bce8ba83cc63df8be9696c42efc9e29c8
robinjoon <0108dlatnqls@gmail.com>
2022-02-21 00:14:35 +0900

    HttpUtil 구조 개편 시도 추가 고민 필요

--- java/tools/HttpUtil.java
+++ java/tools/HttpUtil.java
@@ -1,7 +1,39 @@
 package tools;
+import java.io.IOException;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class HttpUtil {
+	private HttpServletRequest req;
+	private HttpServletResponse rep;
+	private String path;
+	private boolean redirect;
+	
+	public void response() throws IOException {
+		if(redirect) {
+			sendRedirect();
+		}else {
+			forward();
+		}
+	}
+	
+	private void sendRedirect() throws IOException {
+		rep.sendRedirect(path);
+	}
+	
+	private void forward() {
+		try {
+			RequestDispatcher dispatcher = req.getRequestDispatcher(path);
+			dispatcher.forward(req, rep);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void forward(HttpServletRequest request, HttpServletResponse response, String path) {
 		
 		try {
