52942b0113983b30191d0de6f0ba6064e70b1d53
robinjoon <0108dlatnqls@gmail.com>
2022-02-21 00:12:31 +0900

    중복된 AuthManager 사용 삭제

--- java/controller/BoardController.java
+++ java/controller/BoardController.java
@@ -5,13 +5,11 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
+import auth.Auth;
 import auth.AuthManager;
 import service.BoardService;
-import service.MemberService;
 import vo.Board;
-import vo.Permission;
 import tools.HttpUtil;
 public class BoardController implements Controller {
 
@@ -20,12 +18,22 @@ public class BoardController implements Controller {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		try {
+			Auth auth = new Auth(request);
+			
+			if(!AuthManager.loginCheck(auth)) {
+				HttpUtil.forward(request, response, "index.jsp");
+			}else if(!AuthManager.csrfCheck(auth)) {
+				request.setAttribute("err_body", "csrf 토큰이 일치하지 않습니다. 알맞은 접근으로 시도하세요.");
+				request.setAttribute("forward_url", "index.jsp");
+				HttpUtil.forward(request, response, "/WEB-INF/pages/fail.jsp");
+			}
+			
 			if(action.contentEquals("create_board")) {
-				create(request,response);
+				create(request,response,auth);
 			}else if(action.contentEquals("delete_board")) {
-				delete(request,response);
+				delete(request,response,auth);
 			}else if(action.contentEquals("update_board")) {
-				update(request,response);
+				update(request,response,auth);
 			}else {
 				HttpUtil.forward(request, response, "/WEB-INF/pages/fail.jsp");
 			}
@@ -34,109 +42,50 @@ public class BoardController implements Controller {
 		}
 	}
 	
-	private void create(HttpServletRequest request, HttpServletResponse response) 
+	private void create(HttpServletRequest request, HttpServletResponse response, Auth auth) 
 			throws Exception {
-		HttpSession session = request.getSession();
-		String id  = (String)session.getAttribute("id");
-		String board_name=request.getParameter("board_name");
-		Permission rp =	Permission.intToPermission(Integer.parseInt(request.getParameter("rp")));
-		Permission wp = Permission.intToPermission(Integer.parseInt(request.getParameter("wp")));
-		Permission mp = Permission.intToPermission(Integer.parseInt(request.getParameter("mp")));
-		Permission cp = Permission.intToPermission(Integer.parseInt(request.getParameter("cp")));
-		String board_description = request.getParameter("board_description");
-		Board board = new Board();
-		board.setBoard_name(board_name);board.setReadPermission(rp);
-		board.setWritePermission(wp);board.setManagePermission(mp);
-		board.setCommentPermission(cp);
-		board.setBoard_description(board_description);
-		if(AuthManager.loginCheck(request)) { // 로그인 여부 확인
-			if(AuthManager.csrfCheck(request)) { // csrf 토큰 체크
-				if(AuthManager.canMakeBoard(MemberService.getMember(id), board)) { // 게시판 권한설정 확인
-					if(BoardService.createboard(board)) {
-						request.setAttribute("ok_body", "게시판 생성 성공");
-						request.setAttribute("forward_url", "admin.do");
-						HttpUtil.forward(request, response, "/WEB-INF/pages/ok.jsp");
-					}else {
-						request.setAttribute("err_body", "db 에러");
-						request.setAttribute("forward_url", "admin.do");
-						HttpUtil.forward(request, response, "/WEB-INF/pages/fail.jsp");
-					}
-				}else {
-					response.sendRedirect("index.jsp");
-				}
+		Board board = new Board(request);
+		if(AuthManager.canMakeBoard(auth, board)) { // 게시판 권한설정 확인
+			if(BoardService.createboard(board)) {
+				request.setAttribute("ok_body", "게시판 생성 성공");
+				request.setAttribute("forward_url", "admin.do");
+				HttpUtil.forward(request, response, "/WEB-INF/pages/ok.jsp");
 			}else {
-				request.setAttribute("err_body", "csrf 토큰이 일치하지 않습니다. 알맞은 접근으로 시도하세요.");
-				request.setAttribute("forward_url", "index.jsp");
+				request.setAttribute("err_body", "db 에러");
+				request.setAttribute("forward_url", "admin.do");
 				HttpUtil.forward(request, response, "/WEB-INF/pages/fail.jsp");
 			}
-		}else {// 로그인안되어있으면
-			HttpUtil.forward(request, response, "index.jsp");
+		}else {
+			response.sendRedirect("index.jsp");
 		}
 	}
-	private void update(HttpServletRequest request,HttpServletResponse response) throws Exception {
-		HttpSession session = request.getSession();
-		String id  = (String)session.getAttribute("id");
-		String board_name = request.getParameter("board_name");
-		Permission rp =	Permission.intToPermission(Integer.parseInt(request.getParameter("rp")));
-		Permission wp = Permission.intToPermission(Integer.parseInt(request.getParameter("wp")));
-		Permission mp = Permission.intToPermission(Integer.parseInt(request.getParameter("mp")));
-		Permission cp = Permission.intToPermission(Integer.parseInt(request.getParameter("cp")));
-		String board_description = request.getParameter("board_description");
-		Board board = new Board();
-		board.setBoard_name(board_name);board.setReadPermission(rp);
-		board.setWritePermission(wp);board.setManagePermission(mp);
-		board.setCommentPermission(cp);
-		board.setBoard_description(board_description);
-		if(AuthManager.loginCheck(request)) { // 로그인 여부 확인
-			if(AuthManager.csrfCheck(request)) { // csrf 토큰 체크
-				if(AuthManager.canMakeBoard(MemberService.getMember(id), board)) { // 게시판 권한설정 확인
-					if(BoardService.updateboard(board)) {
-						request.setAttribute("ok_body", "게시판 생성 성공");
-						request.setAttribute("forward_url", "admin.do");
-						HttpUtil.forward(request, response, "/WEB-INF/pages/ok.jsp");
-					}else {
-						request.setAttribute("err_body", "db 에러");
-						request.setAttribute("forward_url", "admin.do");
-						HttpUtil.forward(request, response, "/WEB-INF/pages/fail.jsp");
-					}
-				}else {
-					response.sendRedirect("index.jsp");
-				}
+	private void update(HttpServletRequest request,HttpServletResponse response, Auth auth) throws Exception {
+		Board board = new Board(request);
+		if(AuthManager.canMakeBoard(auth, board)) { // 게시판 권한설정 확인
+			if(BoardService.updateboard(board)) {
+				request.setAttribute("ok_body", "게시판 생성 성공");
+				request.setAttribute("forward_url", "admin.do");
+				HttpUtil.forward(request, response, "/WEB-INF/pages/ok.jsp");
 			}else {
-				request.setAttribute("err_body", "csrf 토큰이 일치하지 않습니다. 알맞은 접근으로 시도하세요.");
-				request.setAttribute("forward_url", "index.jsp");
+				request.setAttribute("err_body", "db 에러");
+				request.setAttribute("forward_url", "admin.do");
 				HttpUtil.forward(request, response, "/WEB-INF/pages/fail.jsp");
 			}
-		}else {// 로그인안되어있으면
-			HttpUtil.forward(request, response, "index.jsp");
+		}else {
+			response.sendRedirect("index.jsp");
 		}
 	}
-	private void delete(HttpServletRequest request,HttpServletResponse response) {
-		HttpSession session = request.getSession();
-		String id  = (String)session.getAttribute("id");
-		String board_name = request.getParameter("board_name");
-		Board board = new Board();
-		board.setBoard_name(board_name);
-		if(AuthManager.loginCheck(request)) { //로그인 여부 확인
-			if(AuthManager.canManage(MemberService.getMember(id), board)) {// 권한 확인
-				if(AuthManager.csrfCheck(request)) { // csrf 토큰 체크
-					if(BoardService.deleteBoard(board)) { // 삭제 성공
-						request.setAttribute("ok_body", "게시판 삭제 성공");
-						request.setAttribute("forward_url", "admin.do");
-						HttpUtil.forward(request, response, "/WEB-INF/pages/ok.jsp");
-					}else { //삭제 실패
-						request.setAttribute("err_body", "db 에러");
-						request.setAttribute("forward_url", "admin.do");
-						HttpUtil.forward(request, response, "/WEB-INF/pages/fail.jsp");
-					}
-				}else {
-					request.setAttribute("err_body", "csrf 토큰이 일치하지 않습니다. 알맞은 접근으로 시도하세요.");
-					request.setAttribute("forward_url", "index.jsp");
-					HttpUtil.forward(request, response, "/WEB-INF/pages/fail.jsp");
-				}
-			}else {
-				session.invalidate();
-				HttpUtil.forward(request, response, "index.jsp");
+	private void delete(HttpServletRequest request,HttpServletResponse response, Auth auth) throws Exception {
+		Board board = new Board(request.getParameter("board_name"));
+		if(AuthManager.canManageBoard(auth, board)) {// 권한 확인
+			if(BoardService.deleteBoard(board)) { // 삭제 성공
+				request.setAttribute("ok_body", "게시판 삭제 성공");
+				request.setAttribute("forward_url", "admin.do");
+				HttpUtil.forward(request, response, "/WEB-INF/pages/ok.jsp");
+			}else { //삭제 실패
+				request.setAttribute("err_body", "db 에러");
+				request.setAttribute("forward_url", "admin.do");
+				HttpUtil.forward(request, response, "/WEB-INF/pages/fail.jsp");
 			}
 		}else {
 			HttpUtil.forward(request, response, "index.jsp");
