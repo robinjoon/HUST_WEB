23a4208867d31c9e4b7b5feb362a6ff3c28a98d9
robinjoon <0108dlatnqls@gmail.com>
2022-02-22 22:52:14 +0900

    HttpServletRequest, Post 를 인자로하는 생성자 추가

--- java/vo/Viewlog.java
+++ java/vo/Viewlog.java
@@ -1,6 +1,10 @@
 // 조회수 구현시 새로고침연타로 조회수 올라가는걸 막기 위한 VO
 package vo;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import javax.servlet.http.HttpServletRequest;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +15,12 @@ public class Viewlog implements VO {
 	private String id;
 	private int pid;
 	private Timestamp date;
+	
+	public Viewlog(HttpServletRequest request, Post post) {
+		this.id = (String)request.getSession().getAttribute("id");
+		this.pid = post.getPid();
+		date = Timestamp.valueOf(LocalDateTime.now());
+	}
 	
 	@Override
 	public String toJson() {
