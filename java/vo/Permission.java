e21d9ab1facddfc77b547ddee45c61af45eab310
robinjoon <0108dlatnqls@gmail.com>
2022-02-26 22:52:08 +0900

    무효값, YB,OB 공동관리값 추가

--- java/vo/Permission.java
+++ java/vo/Permission.java
@@ -1,15 +1,17 @@
 package vo;
 
 public enum Permission {
+	INVALID,
 	GUEST,
 	NEWBIE,
 	YB,
 	OB,
 	YB_ADMIN,
 	OB_ADMIN,
-	GENREAL_ADMIN;
+	GENREAL_ADMIN,
+	YB_OB_ADMIN;
 	
-	public static Permission intToPermission(int permission) throws Exception {
+	public static Permission intToPermission(int permission){
 		switch(permission) {
 		case 0:
 			return GUEST;
@@ -25,13 +27,14 @@ public enum Permission {
 			return OB_ADMIN;
 		case 6:
 			return GENREAL_ADMIN;
-			
+		case 7:
+			return YB_OB_ADMIN;
 		default:
-			throw new Exception("invalid permission");
+			return INVALID;
 		}
 	}
 	
-	public static int permissionToInt(Permission permission) throws Exception {
+	public static int permissionToInt(Permission permission){
 		switch(permission) {
 		case GUEST:
 			return 0;
@@ -47,8 +50,10 @@ public enum Permission {
 			return 5;
 		case GENREAL_ADMIN:
 			return 6;
+		case YB_OB_ADMIN:
+			return 7;
 		default:
-			throw new Exception("invalid permission");
+			return -1;
 		}
 	}
 }
